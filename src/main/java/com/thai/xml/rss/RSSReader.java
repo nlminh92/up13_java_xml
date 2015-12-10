67b9b6b0b2101b05c1bce3d2d8004cc4bbc82744
HO Phu Thai <hophuthai@gmail.com>
2015-12-10 22:04:32 +0100

    Java XML - Exercice 2
    Add RSSReader

---
+++ src/main/java/com/thai/xml/rss/RSSReader.java
@@ -0,0 +1,40 @@
+package com.thai.xml.rss;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+public class RSSReader {
+
+    private static final String TEST_URI = "http://www.feedforall.com/sample.xml";
+
+    public static InputStream read() throws ClientProtocolException, IOException {
+        // Ouverture d'un client
+        HttpClient httpClient = HttpClientBuilder.create().build();
+
+        // Création d'une méthode HTTP
+        HttpGet httpGet = new HttpGet(TEST_URI);
+        HttpResponse response;
+        response = httpClient.execute(httpGet);
+        StatusLine statusLine = response.getStatusLine();
+
+        // Invocation de la méthode
+        int statusCode = statusLine.getStatusCode();
+        if (statusCode != HttpStatus.SC_OK) {
+            System.err.println("Method failed : " + response.getStatusLine());
+            return null;
+        }
+
+        // Lecture de la réponse dans un flux
+        InputStream is = response.getEntity().getContent();
+        return is;
+    }
+
+}
--- src/main/java/com/thai/xml/start/Main.java
+++ src/main/java/com/thai/xml/start/Main.java
@@ -2,10 +2,17 @@ package com.thai.xml.start;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
+import org.xml.sax.SAXException;
 
+import com.thai.xml.rss.RSSReader;
 import com.thai.xml.xpath.Marin;
 import com.thai.xml.xpath.XMLUtil;
 
@@ -35,6 +42,19 @@ public class Main {
             return;
         }
         XMLUtil.deserialize(document);
+
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        try {
+            InputStream is = RSSReader.read();
+            RSSHandler rssHandler = new RSSHandler();
+            SAXParser saxParser = factory.newSAXParser();
+            saxParser.parse(is, rssHandler);
+            is.close();
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            XMLUtil.log(e.getMessage());
+            e.printStackTrace();
+        }
+
     }
 
 }
